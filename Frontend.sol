57a90579e90d12856ba9217f598e51470fde6706
hlaure04 <139563015+hlaure04@users.noreply.github.com>
2024-05-12 15:53:26 +0800

    Update Frontend.sol

--- Frontend.sol
+++ Frontend.sol
@@ -4,8 +4,17 @@ pragma solidity ^0.8.0;
 contract PastelUnicorn {
     uint public sparkleCount;
 
+    // Event that will be emitted whenever glitter is sprinkled
+    event GlitterSprinkled(address indexed _from, uint _amount);
+
     function sprinkleGlitter(uint _amount) public {
-        sparkleCount = _amount;
+        // Require that the amount of glitter to be sprinkled is greater than 0
+        require(_amount > 0, "Amount must be greater than 0");
+
+        sparkleCount += _amount;
+
+        // Emit an event after successfully sprinkling glitter
+        emit GlitterSprinkled(msg.sender, _amount);
     }
 
     function countGlitter() public view returns (uint) {

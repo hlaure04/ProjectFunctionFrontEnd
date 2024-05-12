3df31130dca6409fc06c9b93cbd894ce2bfce4ee
hlaure04 <139563015+hlaure04@users.noreply.github.com>
2024-05-12 21:39:56 +0800

    Update Frontend.sol

--- Frontend.sol
+++ Frontend.sol
@@ -4,19 +4,16 @@ pragma solidity ^0.8.0;
 contract PastelUnicorn {
     uint public sparkleCount;
 
-    // Event that will be emitted whenever glitter is sprinkled
-    event GlitterSprinkled(address indexed _from, uint _amount);
-
+    // Function to sprinkle glitter
     function sprinkleGlitter(uint _amount) public {
-        // Require that the amount of glitter to be sprinkled is greater than 0
-        require(_amount > 0, "Amount must be greater than 0");
+        // Require statement checks if the amount is greater than 0
+        // If not, it will revert the transaction and provide the error message
+        require(_amount > 0, "Amount must be greater than 0.");
 
-        sparkleCount += _amount;
-
-        // Emit an event after successfully sprinkling glitter
-        emit GlitterSprinkled(msg.sender, _amount);
+        sparkleCount = _amount;
     }
 
+    // Function to count glitter
     function countGlitter() public view returns (uint) {
         return sparkleCount;
     }
